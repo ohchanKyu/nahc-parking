a551d8f4c15bc8e791a8e0872c44510cdb4c5366
ohchanKyu <okc0202@naver.com>
2025-04-24 17:48:37 +0900

    fix: Fix App Name

--- src/layout/LoadingModal.jsx
+++ src/layout/LoadingModal.jsx
@@ -7,7 +7,7 @@ const LoadingModal = () => {
         <div className={classes.loading_container}>
             <div className={classes.container}>
                 <div className={classes.container_text_box}>
-                    <h1 className={classes.h1_text_bar}> S-Parking </h1>
+                    <h1 className={classes.h1_text_bar}> Parking Mate </h1>
                     <p> 데이터를 처리중입니다. <br/>잠시만 기다려주세요. </p>
                     <img src={loadingImage} alt="loading"/>
                 </div>
